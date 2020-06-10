33e6f3132f35ccfc6b6fbbc65b629baf92a3acf1
Clara <65602079+claraocarroll@users.noreply.github.com>
2020-06-10 18:37:05 +0100

    Update Trial.py

--- Trial.py
+++ Trial.py
@@ -9,7 +9,7 @@ import pysam
 import matplotlib
 matplotlib.use('Agg') #this tells matplotlib we may not necessarily have access to a screen
 from matplotlib import pyplot as plt #this is what we're going to use to plot
-#first define a function that we're going to call this function twice: once for plasmodium and once for cerevisiae
+#first define a function that we're going to call this function for plasmodium 
 def getIndelFreq(fPath):
     #create two empty lists - we're going to fill these up as we go
     plasIns = []
@@ -17,9 +17,9 @@ def getIndelFreq(fPath):
     f = pysam.Samfile('/Users/Clara/Dropbox/barcode2little.bam','rb') #open the bam file
     for record in f:
         
-        #write something that uses the cigar string to calculate the number of insertions in this read
+        #uses the cigar string to calculate the number of insertions in this read
         numOfInsertions = sum(tup[1] for tup in record.cigartuples if tup[0] == 1)
-        #write something that calculates the length of the reference subsequence that this read mapped to
+        #calculates the length of the reference subsequence that this read mapped to
         lenOnRef = record.reference_length 
         #add to the insertions frequency list
         plasIns.append(float(numOfInsertions) / lenOnRef) 
@@ -33,14 +33,11 @@ def getIndelFreq(fPath):
     return plasIns, plasDel
     
 
-
-
-
 #MAIN--------------------------
-#call the function twice - does it have to be a Bam file? (/path/to/plasmodium.bam??) Plasmodium_falciparum.ASM276v2.dna.toplevel.fa
+#call the function twice 
 plasIns, plasDel = getIndelFreq('/Users/Clara/Dropbox/barcode2little.bam')
 
-#first define a function that we're going to call this function twice: once for plasmodium and once for cerevisiae
+# define a function that we're going to call this function  for cerevisiae
 def getIndellFreqq(fPath):
     #create two empty lists - we're going to fill these up as we go
     cerevisiaeIns = []
@@ -62,7 +59,7 @@ def getIndellFreqq(fPath):
     f.close()
     return cerevisiaeIns, cerevisiaeDel
   
-
+#define the cerevisiae variables
 cerevisiaeIns, cerevisiaeDel = getIndellFreqq('/Users/Clara/Dropbox/cerevisiaelittle.bam')
 #do the plotting
 plt.figure() #make a figure to write on
@@ -80,5 +77,5 @@ plt.hist(cerevisiaeDel, 50, alpha = 0.3, label = 'Cerevisiae Del')
 plt.legend(framealpha=0.3) #make it slightly transparent so it doesn't block out any of the bars
 plt.xlabel('InDels Per Reference Base')
 plt.ylabel('Count')
-plt.savefig('barcode2newerplotOut.png') #saves the plot to a file - I like to use pdf's so that I can edit colours with Illustrator/InkScape later if I want, but you can change the file format just by changing the extension.  So 'plotOut.png' will give you a png
+plt.savefig('barcode2newerplotOut.png') #saves the plot to a file 
 plt.close()
